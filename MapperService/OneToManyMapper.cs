61d5b980f2e17d6475a346ae222d72a9aca37c36
ma77c0nd0n <matt.condon@ucdconnect.ie>
2021-05-03 17:45:41 +0100

    implemented one to many mapper add method

--- MapperService/OneToManyMapper.cs
+++ MapperService/OneToManyMapper.cs
@@ -5,17 +5,30 @@ namespace MapperService
 {
     public class OneToManyMapper : IOneToManyMapper
     {
+        private const int MIN_INT = 1;
         private const int MAX_INT = 947483647;
-        private Dictionary<int, IEnumerable<int>> MappingDictionary;
+
+        /// A parent may have multiple children. 
+        /// A child may have only one parent.
+        private readonly Dictionary<int, int> ChildToParentDictionary;
+
+        public OneToManyMapper()
+        {
+            ChildToParentDictionary = new Dictionary<int, int>();
+        }
 
         public void Add(int parent, int child)
         {
-            throw new NotImplementedException();
-        }
-
-        private bool IsValidInput(int parent, int child)
-        {
-            throw new NotImplementedException();
+            if (InputOutOfRange(parent) || InputOutOfRange(child) || child == parent)
+            {
+                throw new ArgumentException($"One or more parameters invalid. Parent: {parent}, Child: {child}");
+            }
+            if (ChildToParentDictionary.ContainsKey(child))
+            {
+                throw new ApplicationException($"Child {child} already has a parent.");
+            }
+            ChildToParentDictionary.Add(child, parent);
+            Console.WriteLine($"Mapping created succesfully. Parent: {parent}, Child: {child}");
         }
 
         public IEnumerable<int> GetChildren(int parent)
@@ -47,5 +60,12 @@ namespace MapperService
         {
             throw new NotImplementedException();
         }
+
+        private bool IsValidInput(int parent, int child)
+        {
+            return true;
+        }
+
+        private bool InputOutOfRange(int input) => input > MAX_INT || input < MIN_INT;
     }
 }
