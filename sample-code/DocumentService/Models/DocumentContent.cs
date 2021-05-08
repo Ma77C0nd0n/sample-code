efebc2fa3da602304faf4448639d626063da7efc
ma77c0nd0n <matt.condon@ucdconnect.ie>
2021-05-08 13:44:05 +0100

    Document Service:  create scaffolding

---
+++ sample-code/DocumentService/Models/DocumentContent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DocumentService.Models
+{
+    public class DocumentContent
+    {
+        public string Client { get; set; }
+        public string DocumentId { get; set; }
+        public IEnumerable<string> Keywords { get; set; }
+    }
+}
---
+++ sample-code/DocumentService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DocumentService
+{
+    public class Program
+    {
+        /* Steps:
+         * For each file in a given directory
+         * Read contents - map to model, validate
+         * Pass mapping properties to lookup store, persist
+         * Delete file
+         */
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
