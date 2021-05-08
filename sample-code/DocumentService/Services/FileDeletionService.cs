0a9d38f3684e4d3dfd7195e404b1a8ff584067cc
ma77c0nd0n <matt.condon@ucdconnect.ie>
2021-05-08 13:44:05 +0100

    Document Service:  create scaffolding

---
+++ sample-code/DocumentService/Services/FileDeletionService.cs
@@ -0,0 +1,19 @@
+﻿using DocumentService.Models;
+using System.Threading.Tasks;
+
+namespace DocumentService.Services
+{
+
+    public interface IFileDeletionService
+    {
+        Task DeleteFileAsync(string fileName);
+    }
+
+    public class FileDeletionService : IFileDeletionService
+    {
+        public Task DeleteFileAsync(string fileName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
